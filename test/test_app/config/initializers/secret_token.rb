361869bcf8990ffd51a36666827ae6e86d06cd57
Jason Frey <jfrey@redhat.com>
2014-02-25 17:02:42 -0500

    Add basic Rails 4 app for testing.

---
+++ test/test_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '4fb7870eae61c26f896fbb7fd5b17b78d37a6701fd863b9b27ab53c94c418e12fe3961cafa3c8e8380fd3afef6cf8fb772be3a5861d0ce73305852e425b0bb79'
